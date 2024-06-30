345215eb1f0d619ac50f3a67fa2f3cfae6f3de97
Mantresh <ray779@tuta.io>
2024-07-01 01:22:07 +0530

    complete: window lamp

---
+++ src/window-lamp/window.cpp
@@ -0,0 +1,37 @@
+#include <print>
+#include <string_view>
+#include <source_location>
+#include <exception>
+#include <cmath>
+
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#include <spdlog/spdlog.h>
+
+#include <wayland-client.h>
+#include "wayland/xdg-shell.h"
+
+#include "app.inl"
+
+int main()
+{
+	spdlog::set_level(spdlog::level::debug);
+	spdlog::set_pattern("[%^%l%$ +%o] %v");
+
+	App app;
+	try
+	{
+		app.initialize();
+		app.run();
+	}
+	catch (const App::assertion&)
+	{
+		return 1;
+	}
+	catch (const std::exception& e)
+	{
+		std::println(stderr, "Fatal std::exception: {}", e.what());
+		return 2;
+	}
+}
